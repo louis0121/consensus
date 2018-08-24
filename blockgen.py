2629a5dd9c04e542c2b8f6673de8516977599683
louis0121 <louis0121@126.com>
2018-08-24 23:57:19 +0800

    Add multiprocessing file multiprotps.py

--- blockgen.py
+++ blockgen.py
@@ -10,166 +10,167 @@ from consistent import BLOCKGEN_POINT, IDCONST, CHAIN_SHOWTIME
 
 # Block generation process
 class BlockGeneration(threading.Thread):
-	def __init__(self, logdirectory):
-		threading.Thread.__init__(self)
-		self.unit = BLOCKGEN_POINT
-		self.logdirectory = logdirectory
-		
-	def run(self):
-		filename = self.logdirectory + 'msghandlelog.txt'
-		self.logger = logging.getLogger(__name__)
-		self.logger.setLevel(level = logging.INFO)
-		handler = logging.FileHandler(filename)
-		handler.setLevel(logging.INFO)
-		formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
-		handler.setFormatter(formatter)
-		self.logger.addHandler(handler)
-		
-		self.logger.info('Start ID block generation process')
-		try:
-#			timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'Start ID block generation process'
-#			print(timelog)
-			cur_time = int(time.time())
-			prev_time = cur_time - ( cur_time % BLOCKGEN_POINT )
-			notstart = True
-			while notstart:
-				cur_time = int(time.time())
-				if cur_time > prev_time + BLOCKGEN_POINT:
-#					timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'ID Block generation start...'
-#					print(timelog)
-					self.logger.info('ID Block generation start...')
-					notstart = False
-				else:
-					time.sleep(1)
-					
-			cur_time = int(time.time())
-			prev_time = cur_time - ( cur_time % self.unit )
-			while True:
-				cur_time = int(time.time())
-				date_stamp = ( cur_time - ( cur_time % self.unit ) )
-				if date_stamp > prev_time:
-					generatethread = threading.Thread(target=self.__genidblock)
-					generatethread.start()
-					prev_time = date_stamp
-				else:
-					time.sleep(1)
-			
-		except Exception as e:
-			self.logger.info(e)
-			
-	def __genidblock(self):
-#		timelog = time.strftime('\n%Y-%m-%d %H:%M:%S ', time.localtime()) + 'Generate an id block'
-#		print(timelog)
-		self.logger.info('Generate an id block')
-		timestamp = time.time()
-		glovar.threadLock.acquire()
-		logcontent = '\nglovar.Pool_isbackup:' + str(glovar.Pool_isbackup)
-		self.logger.info(logcontent)
-		if glovar.Pool_isbackup:
-			idgeneration_pool = glovar.BACK_POOL.copy()
-			prevblockhash = glovar.BACKPREV_BLOCKHASH
-			blockheight = glovar.BACKMINEDBLOCK_HEIGHT
-			blocktarget = glovar.BACKMINING_TARGET
-			glovar.Generating_height = blockheight + 1
-			glovar.Pool_isbackup = False
-		else:
-			idgeneration_pool = glovar.IDENTITY_POOL.copy()
-			prevblockhash = glovar.PREV_BLOCKHASH
-			blockheight = glovar.MINEDBLOCK_HEIGHT
-			blocktarget = glovar.MINING_TARGET
-			glovar.Generating_height = blockheight + 1
-		glovar.threadLock.release()
+        def __init__(self, logdirectory):
+                threading.Thread.__init__(self)
+                self.unit = BLOCKGEN_POINT
+                self.logdirectory = logdirectory
+                
+        def run(self):
+                filename = self.logdirectory + 'msghandlelog.txt'
+                self.logger = logging.getLogger(__name__)
+                self.logger.setLevel(level = logging.INFO)
+                handler = logging.FileHandler(filename)
+                handler.setLevel(logging.INFO)
+                formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+                handler.setFormatter(formatter)
+                self.logger.addHandler(handler)
+                
+                self.logger.info('Start ID block generation process')
+                try:
+#                       timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'Start ID block generation process'
+#                       print(timelog)
+                        cur_time = int(time.time())
+                        prev_time = cur_time - ( cur_time % BLOCKGEN_POINT )
+                        notstart = True
+                        while notstart:
+                                cur_time = int(time.time())
+                                if cur_time > prev_time + BLOCKGEN_POINT:
+#                                       timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'ID Block generation start...'
+#                                       print(timelog)
+                                        self.logger.info('ID Block generation start...')
+                                        notstart = False
+                                else:
+                                        time.sleep(1)
+                                        
+                        cur_time = int(time.time())
+                        prev_time = cur_time - ( cur_time % self.unit )
+                        while True:
+                                cur_time = int(time.time())
+                                date_stamp = ( cur_time - ( cur_time % self.unit ) )
+                                if date_stamp > prev_time:
+                                        generatethread = threading.Thread(target=self.__genidblock)
+                                        generatethread.start()
+                                        prev_time = date_stamp
+                                else:
+                                        time.sleep(1)
+                        
+                except Exception as e:
+                        self.logger.info(e)
+                        
+        def __genidblock(self):
+#               timelog = time.strftime('\n%Y-%m-%d %H:%M:%S ', time.localtime()) + 'Generate an id block'
+#               print(timelog)
+                self.logger.info('Generate an id block')
+                timestamp = time.time()
+                glovar.threadLock.acquire()
+                logcontent = '\nglovar.Pool_isbackup:' + str(glovar.Pool_isbackup)
+                self.logger.info(logcontent)
+                if glovar.Pool_isbackup:
+                        idgeneration_pool = glovar.BACK_POOL.copy()
+                        prevblockhash = glovar.BACKPREV_BLOCKHASH
+                        blockheight = glovar.BACKMINEDBLOCK_HEIGHT
+                        blocktarget = glovar.BACKMINING_TARGET
+                        glovar.Generating_height = blockheight + 1
+                        glovar.Pool_isbackup = False
+                else:
+                        idgeneration_pool = glovar.IDENTITY_POOL.copy()
+                        prevblockhash = glovar.PREV_BLOCKHASH
+                        blockheight = glovar.MINEDBLOCK_HEIGHT
+                        blocktarget = glovar.MINING_TARGET
+                        glovar.Generating_height = blockheight + 1
+                glovar.threadLock.release()
 
-		str_convert = ''
-		for each in idgeneration_pool:
-			str_convert += ''.join(str(x) for x in each)
-		pool_idnumber = len(idgeneration_pool)
+                str_convert = ''
+                for each in idgeneration_pool:
+                        str_convert += ''.join(str(x) for x in each)
+                pool_idnumber = len(idgeneration_pool)
 
-		temp = str(prevblockhash) + str_convert + str(timestamp)
-		hashvalue = int(hashlib.sha256(temp.encode('utf-8')).hexdigest(), 16)
-		newblock = [blockheight, timestamp, blocktarget, prevblockhash, hashvalue, pool_idnumber, idgeneration_pool]
-#		print('blockheight:', newblock[0], '\n timstap: ', newblock[1], '\n target: ',newblock[2], '\n prevhash: ', newblock[3], '\n blockhash: ', newblock[4], '\n idnumber: ', newblock[5])
-		logcontent = '\nblockheight:' + str(newblock[0]) + '\ntimstap: ' + str(newblock[1]) + '\ntarget: ' + str(newblock[2]) + '\nprevhash: ' + str(newblock[3]) + '\nblockhash: ' + str(newblock[4]) + '\nidnumber: ' + str(newblock[5])
-		self.logger.info(logcontent)
-		
-		glovar.threadLock.acquire()
-		glovar.blockqueue.put(newblock)
-		glovar.threadLock.release()
+                temp = str(prevblockhash) + str_convert + str(timestamp)
+                hashvalue = int(hashlib.sha256(temp.encode('utf-8')).hexdigest(), 16)
+                newblock = [blockheight, timestamp, blocktarget, prevblockhash, hashvalue, pool_idnumber, idgeneration_pool]
+#               print('blockheight:', newblock[0], '\n timstap: ', newblock[1], '\n target: ',newblock[2], '\n prevhash: ', newblock[3], '\n blockhash: ', newblock[4], '\n idnumber: ', newblock[5])
+                logcontent = '\nblockheight:' + str(newblock[0]) + '\ntimstap: ' + str(newblock[1]) + '\ntarget: ' + str(newblock[2]) + '\nprevhash: ' + str(newblock[3]) + '\nblockhash: ' + str(newblock[4]) + '\nidnumber: ' + str(newblock[5])
+                self.logger.info(logcontent)
+                
+                glovar.threadLock.acquire()
+                glovar.blockqueue.put(newblock)
+                glovar.threadLock.release()
 
 # Log the state of IDBLOCKCHAIN
 class BlockchainShow(threading.Thread):
-	def __init__(self, logdirectory):
-		threading.Thread.__init__(self)
-		self.logdirectory = logdirectory
-		
-	def run(self):
-		self.logger = logging.getLogger(__name__)
-#		timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBlockchain output to chainlog.txt is started...'
-#		print(timelog)
-		self.logger.info('IDBlockchain adn IDkeychain log proecess is started')
-		
-		try:
-			filename = self.logdirectory + 'chainstatelog.txt'
-			f = open(filename, "w")
-			
-			IDkeyname = self.logdirectory + 'idkeychain.txt'
-			fid = open(IDkeyname, "w")
-			
-#			timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'Start IDBLOCKCHAIN logfile process'
-#			print(timelog)
-			cur_time = int(time.time())
-			prev_time = cur_time - ( cur_time % BLOCKGEN_POINT ) + CHAIN_SHOWTIME
-			notstart = True
-			while notstart:
-				cur_time = int(time.time())
-				if cur_time > prev_time + BLOCKGEN_POINT:
-#					timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBLOCKCHAIN logfile process start...'
-#					print(timelog)
-					self.logger.info('Start IDBlockchain adn IDkeychain log proecess...')
-					notstart = False
-				else:
-					time.sleep(1)
-					
-			prev_time += BLOCKGEN_POINT
-			while True:
-				date_stamp = int(time.time())
-				if date_stamp > prev_time:
-					timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDKEYCHAIN:' + '\n'
-					
-					fid.write(timelog)
-					for each in glovar.IDKEYCHAIN:
-						count = 0
-						for every in each:
-							term = str(every[5]) + ' ' + str(every[1]) + '\n'
-							fid.write(term)
-							count += 1
-						term = str(count) + '\n\n'
-						fid.write(term)
-						
-					timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBLOCKCHAIN:' + '\n'
-					f.write(timelog)
-					for each in glovar.IDBLOCKCHAIN:
-						timelog = ''
-						for i in range(6):
-							timelog = timelog + str(each[i]) + '\n'
-						f.write(timelog)
-						timelog = ''
-						for i in range(each[5]):
-							timelog = timelog + str(each[6][i][1]) + '\n'
-						f.write(timelog)
-						f.write('\n')
-						
-					prev_time += BLOCKGEN_POINT
-				else:
-					time.sleep(2)
-			
-		except Exception as e:
-			self.logger.info(e)
-		
-		finally:
-			f.close()
+        def __init__(self, logdirectory):
+                threading.Thread.__init__(self)
+                self.logdirectory = logdirectory
+                
+        def run(self):
+                self.logger = logging.getLogger(__name__)
+#               timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBlockchain output to chainlog.txt is started...'
+#               print(timelog)
+                self.logger.info('IDBlockchain adn IDkeychain log proecess is started')
+                
+                try:
+                        filename = self.logdirectory + 'chainstatelog.txt'
+                        f = open(filename, "w")
+                        
+                        IDkeyname = self.logdirectory + 'idkeychain.txt'
+                        fid = open(IDkeyname, "w")
+                        
+#                       timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'Start IDBLOCKCHAIN logfile process'
+#                       print(timelog)
+                        cur_time = int(time.time())
+                        prev_time = cur_time - ( cur_time % BLOCKGEN_POINT ) + CHAIN_SHOWTIME
+                        notstart = True
+                        while notstart:
+                                cur_time = int(time.time())
+                                if cur_time > prev_time + BLOCKGEN_POINT:
+#                                       timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBLOCKCHAIN logfile process start...'
+#                                       print(timelog)
+                                        self.logger.info('Start IDBlockchain adn IDkeychain log proecess...')
+                                        notstart = False
+                                else:
+                                        time.sleep(1)
+                                        
+                        prev_time += BLOCKGEN_POINT
+                        while True:
+                                date_stamp = int(time.time())
+                                if date_stamp > prev_time:
+                                        timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDKEYCHAIN:' + '\n'
+                                        
+                                        fid.write(timelog)
+                                        for each in glovar.IDKEYCHAIN:
+                                                count = 0
+                                                for every in each:
+                                                        term = str(every[5]) + ' ' + str(every[1]) + '\n'
+                                                        fid.write(term)
+                                                        count += 1
+                                                term = str(count) + '\n\n'
+                                                fid.write(term)
+                                                
+                                        timelog = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime()) + 'IDBLOCKCHAIN:' + '\n'
+                                        f.write(timelog)
+                                        for each in glovar.IDBLOCKCHAIN:
+                                                timelog = ''
+                                                for i in range(6):
+                                                        timelog = timelog + str(each[i]) + '\n'
+                                                f.write(timelog)
+                                                timelog = ''
+                                                for i in range(each[5]):
+                                                        timelog = timelog + str(each[6][i][1]) + '\n'
+                                                f.write(timelog)
+                                                f.write('\n')
+                                                
+                                        prev_time += BLOCKGEN_POINT
+                                else:
+                                        time.sleep(2)
+                        
+                except Exception as e:
+                        self.logger.info(e)
+                
+                finally:
+                        f.close()
+                        fid.close()
 
 # function test
 if __name__ == '__main__':
-	blockgenprocess = BlockGeneration()
-	blockgenprocess.start()
+        blockgenprocess = BlockGeneration()
+        blockgenprocess.start()
---
+++ multiprotps.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+
+import sys, os, time, threading, logging, configparser#, pdb
+import multiprocessing
+import queue
+
+import bitcoin.rpc
+from bitcoin.wallet import CBitcoinAddress
+from bitcoin.core import b2lx, lx, COIN
+
+class MeasureProcessing(multiprocessing.Process):
+    def __init__(self, sendclient, txnumber, sendnum, fname, lock):
+        multiprocessing.Process.__init__(self)
+        self.sendclient = sendclient
+        self.txnumber = txnumber
+        self.sendnum = sendnum
+        self.fname = fname
+        self.lock = lock
+
+    def run(self):
+        global teststate
+        global testover
+        pwd = os.getcwd()
+        clientconf = pwd + '/log/' + str(self.sendclient+self.sendnum) + '/bitcoin.conf'
+        Bclientproxy = bitcoin.rpc.Proxy(btc_conf_file = clientconf)
+        clientconf = pwd + '/log/' + str(self.sendclient) + '/bitcoin.conf'
+        Aclientproxy = bitcoin.rpc.Proxy(btc_conf_file = clientconf)
+
+        syncname = pwd + '/log/' + self.fname
+        filename = pwd + '/log/tpsdata' + str(self.sendclient) +'.txt'
+        self.logger = logging.getLogger(str(self.sendclient))
+        self.logger.setLevel(level = logging.INFO)
+        handler = logging.FileHandler(filename)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.info('-----------------------------------------------------------')
+        self.logger.info('Start tps measurement process!')
+
+        transno = self.txnumber
+        addr_list = []
+        logcontent = 'client:' + str(self.sendclient+self.sendnum) + ' generate retrieving addressess...'
+        self.logger.info(logcontent)
+        #print('client:', self.sendclient+3, ' generate retrieving addresses...')
+        starttime = time.time()
+        for i in range(transno):
+            addr_list.append(Bclientproxy.call('getnewaddress'))
+
+        preblockheight = Bclientproxy.getblockcount()
+        
+        logcontent = 'client:' + str(self.sendclient) + ' send transactions...'
+        self.logger.info(logcontent)
+        #print('client:', self.sendclient, ' send transactions...')
+        for each in addr_list:
+            txid = Aclientproxy.sendtoaddress(each, 0.01 * COIN)
+        
+        #pdb.set_trace()
+        #print('COIN:', COIN)
+
+        endtime = time.time()
+        timelength = endtime - starttime
+        logcontent = 'client:' + str(self.sendclient) + ' takes ' + str(timelength) + ' seconds to send transactions.'
+        self.logger.info(logcontent)
+        #print('client:', self.sendclient, 'takes', timelength, 'seconds.')
+
+        logcontent = 'client:' + str(self.sendclient) + ' waiting for last transaction confirmation...'
+        self.logger.info(logcontent)
+
+        self.lock.acquire()
+        fs = open(syncname, 'r')
+        line = fs.readline()
+        print("line:", line)
+        resu = int(line) + 1
+        output = str(resu)
+        print("output:", output)
+        fs.close()
+        fs = open(syncname, 'w')
+        fs.write(output)
+        fs.close()
+        self.lock.release()
+        print("Process:", self.sendclient)#, "teststate[", self.sendclient-1, "]:", teststate[self.sendclient-1])
+        #print('client:', self.sendclient, ' last transaction id:', txid)
+        #print('client:', self.sendclient, ' waiting last transaction confirmation...')
+        notconfirm = True
+        while notconfirm:
+            txinfo = Aclientproxy.gettransaction(txid)
+            if txinfo['confirmations'] == 0:
+                time.sleep(1)
+            else:
+                lastblockheight = Aclientproxy.getblockcount()
+                break
+            
+        maxtx = 0
+        logcontent = 'client:' + str(self.sendclient) + ' find the block with most transactions...\n'
+        self.logger.info(logcontent)
+        #print('client:', self.sendclient, ' find the block with most transactions...')
+        for i in range(preblockheight, lastblockheight+1, 1):
+            blockhash = Aclientproxy.call('getblockhash', i)
+            blockcontent = Aclientproxy.call('getblock', blockhash)
+            logcontent = 'blockheight:' + str(i) +', tx number:' + str(len(blockcontent['tx']))
+            self.logger.info(logcontent)
+            #print('client:', self.sendclient, ' find: blockheight: ', i, '   tx number:', len(blockcontent['tx']))
+            if len(blockcontent['tx']) - 1 > maxtx:
+                maxtx = len(blockcontent['tx']) - 1
+
+        logcontent = 'client:' + str(self.sendclient) + ' find maxtx:' + str(maxtx)
+        self.logger.info(logcontent)
+        #print('client:', self.sendclient, ' find maxtx:', maxtx)
+        
+        tpsmeasure = maxtx / 3
+        logcontent = 'client:' + str(self.sendclient) + ' find tpsmax:' + str(tpsmeasure)
+        self.logger.info(logcontent)
+
+        self.lock.acquire()
+        fs = open(syncname, 'r')
+        line = fs.readline()
+        print("line:", line)
+        resu = int(line) + 1
+        output = str(resu)
+        print("output:", output)
+        fs.close()
+        fs = open(syncname, 'w')
+        fs.write(output)
+        fs.close()
+        self.lock.release()
+        #print('client:', self.sendclient, 'find tpsmax:', tpsmeasure)
+
+# main function
+def main():
+    pwd = os.getcwd()
+    cf = configparser.ConfigParser()
+    cfpath = pwd + '/measure.ini'
+    cf.read(cfpath)
+
+    trannum = int(cf.get('measureconf', 'sendtrannum'))
+    sendnum = int(cf.get('measureconf', 'sendnodenum'))
+    
+    txnumber = trannum // sendnum
+    
+    fname = "notify.txt"
+    syncname = pwd + '/log/' + fname
+    lock = multiprocessing.Lock()
+    lock.acquire()
+    fs = open(syncname, 'w')
+    fs.write("0")
+    fs.close()
+    lock.release()
+
+    for i in range(sendnum):
+        meaclient = MeasureProcessing(i+1,txnumber,sendnum,fname,lock)
+        meaclient.start()
+    
+    notready = True
+    while notready:
+        lock.acquire()
+        fs = open(syncname, 'r')
+        line = fs.readline()
+        lock.release()
+        if int(line) == sendnum:
+            print("start service.")
+            break
+        else:
+            time.sleep(5)
+
+
+    time.sleep(10)
+#    print('start service.')
+    os.system("./service.sh")
+
+    while notready:
+        lock.acquire()
+        fs = open(syncname, 'r')
+        line = fs.readline()
+        lock.release()
+        if int(line) == 2*sendnum:
+            break
+        else:
+            time.sleep(5)
+            print("line:", int(line))
+
+#    print('end service.')
+    os.system("./endservice.sh")
+
+# function test         
+if __name__ == '__main__':
+    main()
